a24e555b22f1b8bde904bf48e35ff91ee664d320
Ravi Shivam Autar <raviautar27@gmail.com>
2018-09-01 09:29:51 +0200

    Cashback after transaction complete

--- src/main.py
+++ src/main.py
@@ -11,13 +11,21 @@ from src.libs.piggybunq_lib import parse_user_discounts, determine_discount
 def refresh_database(bunq, discounts):
     database = DbHelper()
     existing_tx = [tranx[0] for tranx in database.get_payments_from_database()]
+    history = bunq.get_all_payment(count=200)
+    for h in history:
+        if h.description.split("-")[0] != "CASHBACK":
+            _, dsc = determine_discount(h.description, discounts)
+            database.add_payment_to_database(h.id_, h.description, h.amount.value, dsc*h.amount.value)
+            existing_tx.append(h.id_)
+
     while True:
         new_payments = bunq.get_all_payment(5)
         for np in new_payments:
             if np.id_ not in existing_tx and np.description.split("-")[0] != "CASHBACK":
-                _, dsc = determine_discount(np.description, discounts)
-                database.add_payment_to_database(np.id_, np.description, np.amount.value, dsc)
-                pass
+                shop , dsc = determine_discount(np.description, discounts)
+                database.add_payment_to_database(np.id_, np.description, np.amount.value, dsc*np.amount.value)
+                existing_tx.append(np.id_)
+                bunq.make_request(dsc, "-".join("CASHBACK", shop, dsc), "sugardaddy@bunq.com")
         time.sleep(3)
 
 
@@ -34,6 +42,8 @@ def main():
     discounts = parse_user_discounts("data/discount.csv")
 
     database = DbHelper()
+
+
 
     Thread(target=refresh_database, args=(bunq, discounts)).start()
 
